4d4d0dac6c8ed15e277a0e209ff6c7db0e0043c7
syh1998 <1760148285@qq.com>
2017-12-11 11:29:59 +0800

    xiugai

--- swiper4/js/script.js
+++ swiper4/js/script.js
@@ -17,3 +17,17 @@ var swiper = new Swiper('.swiper-container', {
         }
     }
 });
+
+var mp3=document.querySelector("#mp3");
+var mp3Btn=document.querySelector("#mp3Btn");
+mp3Btn.onclick = function () {
+    if (mp3.paused) {
+        mp3Btn.style.animationPlayState = 'running';
+        mp3.play();
+    } else {
+        mp3Btn.style.animationPlayState = 'paused';
+        mp3.pause();
+    }
+}
+
+
