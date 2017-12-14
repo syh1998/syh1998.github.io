1211166f2ab10ef3246c97745f064145be988785
syh1998 <1760148285@qq.com>
2017-12-14 09:51:46 +0800

    tianjia

---
+++ 双选会/js/script.js
@@ -0,0 +1,31 @@
+var swiper = new Swiper('.swiper-container', {
+    direction: 'vertical',
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next'
+    },
+    on:{
+        init: function(){
+            swiperAnimateCache(this); //隐藏动画元素
+            swiperAnimate(this); //初始化完成开始动画
+        },
+        slideChangeTransitionEnd: function(){
+            swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
+        }
+    }
+});
+
+var mp3=document.querySelector("#mp3");
+var mp3Btn=document.querySelector("#mp3Btn");
+mp3Btn.onclick = function () {
+    if (mp3.paused) {
+        mp3Btn.style.animationPlayState = 'running';
+        mp3.play();
+    } else {
+        mp3Btn.style.animationPlayState = 'paused';
+        mp3.pause();
+    }
+};
